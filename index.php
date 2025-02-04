e0748c02fab664f42d423cb7f469ab329b2d9e05
Ramadhan_16 <142005525+Ramadhan16-liv@users.noreply.github.com>
2025-02-04 11:42:58 +0800

    selesai

--- index.php
+++ index.php
@@ -8,35 +8,37 @@
         <h2>Kalkulator BMI</h2>
         <form method="post" action="">
             <input type="number" step="any" name="Bbadan" placeholder="Masukan Berat Badan" required>
-            <input type="number" step="any" name="Tbadan" placeholder="Masukan Tinggi Badan" required>
-            <button type="submit" name="bmi" value="submit">Hitung</button> <br>
+            <input type="number" step="any" name="Tbadan" placeholder="Masukan Tinggi Badan (meter)" required>
+            <button type="submit" name="submit" value="submit">Hitung</button> <br>
         </form>
     </div>   
 
     <?php
-    if (isset($_POST['bmi'])) {
+    if (isset($_POST['submit'])) {
         $Bbadan = $_POST['Bbadan'];
         $Tbadan = $_POST['Tbadan'];
-        $bmi = $Bbadan / ($Tbadan * $Tbadan);
+        $bmi = 0; 
 
-        if ($Bbadan = !0 && $Tbadan = !0 ) 
-        
+        if ($Bbadan != 0 && $Tbadan != 0) {
+            $bmi = $Bbadan / ($Tbadan * $Tbadan);
+            echo "<h2>Berat badan: $Bbadan kg, Tinggi badan: $Tbadan m, BMI: $bmi</h2>"; 
 
-        if ($bmi < 17.0) {
-            print ("Kurus, kekurangan berat badan berat");
-        } elseif ($bmi < 18.4) {
-            print ("Kurus, kekurangan berat badan ringan");
-        } elseif ($bmi < 25.0) {
-            print ("Normal");
-        } elseif ($bmi < 27.0) {
-            print ("Gemuk, Kelebihan berat badan tingkat ringan");
+            // Menentukan kategori BMI
+            if ($bmi < 17.0) {
+                print ("Kurus, kekurangan berat badan berat");
+            } elseif ($bmi < 18.4) {
+                print ("Kurus, kekurangan berat badan ringan");
+            } elseif ($bmi < 25.0) {
+                print ("Normal");
+            } elseif ($bmi < 27.0) {
+                print ("Gemuk, kelebihan berat badan tingkat ringan");
+            } else {
+                print ("Gemuk, kelebihan berat badan tingkat berat");
+            } 
         } else {
-            print ("Gemuk, Kelebihan berat badan tingkat berat");
-        } 
-        echo "bmi: $bmi";
+            echo "<h2>Berat dan tinggi badan harus lebih dari nol!</h2>";
         }
-
-    
+    }
     ?>
 
 </body>
